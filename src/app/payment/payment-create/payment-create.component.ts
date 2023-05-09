8040fa94076b771d9c833f60ce9631b64f643100
nidhal19615 <nidhal.sassi@esprit.tn>
2023-05-09 17:42:17 +0200

    final demo

--- src/app/payment/payment-create/payment-create.component.ts
+++ src/app/payment/payment-create/payment-create.component.ts
@@ -4,6 +4,7 @@ import {Donation, Payment} from "../../model/donation";
 import {DonationService} from "../../service/donation.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ToastrService} from "ngx-toastr";
+import { PaymentService } from 'src/app/service/payment.service';
 interface Type {
   value: string;
   viewValue: string;
@@ -19,6 +20,7 @@ export class PaymentCreateComponent implements OnInit {
   default_amount: number = 0;
   form!: FormGroup;
   Payment!:Payment
+  id:any
   type: Type[] = [
     {value: 'VIR', viewValue: 'VIR'},
     {value: 'CASH', viewValue: 'CASH'},
@@ -26,6 +28,7 @@ export class PaymentCreateComponent implements OnInit {
   ];
   constructor(
     public donationsService: DonationService,
+    private PaymentService :PaymentService,
     private router: Router,
     private fb: FormBuilder,
     private route: ActivatedRoute,
@@ -34,6 +37,10 @@ export class PaymentCreateComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( paramMap => {
+      this.id = paramMap.get('id');
+  })
+
     this.Payment = new Payment()
     this.form = new FormGroup({
       amount: new FormControl('', [Validators.required]),
@@ -42,6 +49,7 @@ export class PaymentCreateComponent implements OnInit {
       status: new FormControl('', Validators.required),
       raison: new FormControl('', Validators.required),
     });
+    console.log(this.id)
   }
 
 
@@ -55,8 +63,17 @@ export class PaymentCreateComponent implements OnInit {
     this.Payment.amount=this.form.get('amount')?.value
     this.Payment.date=this.form.get('date')?.value
     this.toastr.success('Payment Created', 'Payment');
-    console.log(this.Payment)
-
+    this.PaymentService.add(this.Payment,this.id).subscribe(res=>{
+      
+    })
+    if(this.Payment.type =="VIR")  {
+      window.open("https://buy.stripe.com/test_7sI4iYfv47hdgoMcMM", "_blank");
+      this.router.navigate(['user/main/listPayment']);
+    }else{
+    
+      this.router.navigate(['user/main/listPayment']);
+    }
+   
   }
 
 }
